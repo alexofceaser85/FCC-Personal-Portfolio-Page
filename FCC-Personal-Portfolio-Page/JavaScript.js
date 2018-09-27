dbd04427f5e4674dd8d0fe1ab10efba9fac0fe8f
Alex <alexofcesare@gmail.com>
2018-09-26 22:48:37 -0400

    Added Ability to Copy Email to Clipboard

--- FCC-Personal-Portfolio-Page/JavaScript.js
+++ FCC-Personal-Portfolio-Page/JavaScript.js
@@ -1,3 +1,6 @@
-﻿function CopyFunction(){
+﻿function CopyFunction() {
+    var CopyEmail = document.getElementById("EmailInput");
+    CopyEmail.select();
+    document.execCommand("copy");
     alert("Email Copied to Clipboard");
 }
