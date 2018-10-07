b576faa3b2807f44ac02f5c264d3b7b07be9fc58
Alex <alexofcesare@gmail.com>
2018-10-07 15:33:39 -0400

    Added Mobile Nav Bar

--- FCC-Personal-Portfolio-Page/JavaScript.js
+++ FCC-Personal-Portfolio-Page/JavaScript.js
@@ -1,6 +1,20 @@
 ﻿function CopyFunction() {
     var CopyEmail = document.getElementById("EmailInput");
-    CopyEmail.select();
+    CopyEmail.select(HTMLInputElement);
     document.execCommand("copy");
     alert("Email Copied to Clipboard");
+}
+function DisplayNavBar() {
+    Counter = 0;
+    var x = document.getElementsByClassName("NavLink");
+    for (Counter; Counter < x.length; Counter++) {
+        x[Counter].classList.toggle("navLinkFunction");
+    }
+
+    document.getElementById("NavCont").classList.toggle("navFlexContainerFunction");
+    //classList.toggle("navFlexContainerFunction");
+    document.getElementById("NavBar").classList.toggle("navBarFunction");
+}
+function ShiftContent() {
+    document.getElementById("Welcome").classList.toggle("shiftContentFunction");
 }
